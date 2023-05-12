3b6d78dada07c951154b03fee66de79405a96d77
nitin <nitinroy1209@gmail.com>
2023-05-12 13:10:06 +0530

    addition

---
+++ src/basics/AdditionProgram.java
@@ -0,0 +1,13 @@
+package basics;
+	public class AdditionProgram {
+	    public static void main(String[] args) {
+	        int num1 = 5;
+	        int num2 = 10;
+	        
+	        int sum = num1 + num2;
+	        
+	        System.out.println("The sum is: " + sum);
+	    }
+	}
+
+
