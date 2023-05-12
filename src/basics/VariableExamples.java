93836295ba1a0e4269dfc6fc53bceb4df1185e2a
nitin <nitinroy1209@gmail.com>
2023-05-12 12:43:11 +0530

    variable program

---
+++ src/basics/VariableExamples.java
@@ -0,0 +1,32 @@
+package basics;
+class VariableExamples 
+{
+	public static void main(String args[]) {
+		int ticketCost;
+		int freeTicket = 0;
+		double circlePi = 3.14159;
+		short serialNumber = 10;
+		char category = 'A';
+		String customerName = "SACHIN";
+		long serialdigits = -9223;
+		float discountRate = 0.01f;
+
+		ticketCost = 15000;
+
+		System.out.println(ticketCost);
+
+		System.out.println("The cost of the ticket is " + ticketCost);
+
+		System.out.println("Circle Pi value: " + circlePi);
+		System.out.println("Serial Number: " + serialNumber);
+		System.out.println("Category: " + category);
+		System.out.println("Customer Name: " + customerName);
+		System.out.println("Serial Digits: " + serialdigits);
+		System .out .println("Discount is: " + discountRate);
+		System.out.println("Ticket is free for children: " + freeTicket);
+	}
+}
+
+
+
+
---
+++ src/basics/VariablesEx.java
@@ -0,0 +1,26 @@
+package basics;
+
+class VariablesEx {
+	public static void main(String args[])
+	{
+		
+		//camelCasing( variable names, methodnames)
+		int ticketPrice;
+		
+		double pi=3.14;
+		
+		short SNo=1;
+		
+		char gender='M';
+		
+		String name="Nitin";
+		
+		ticketPrice=9000;
+		
+		System.out.println(ticketPrice);
+		
+		System.out.println("The price of the ticket is "+ticketPrice);
+		
+	}
+
+}
